e46ccf780e88bb11fa1f6788f4ad35d42ccdc807
Fernanda Cidrim <mfcidrimlopes@gmail.com>
2026-07-27 16:08:20 +0100

    Teste da inicialização do baralho e do shuffle a funcionar na main

--- src/game_state.c
+++ src/game_state.c
@@ -4,12 +4,13 @@ Card *fill_deck(Pile *pile) {
 	if (pile == NULL)
 		return NULL;
 	Card *deck = malloc(52 * sizeof(*deck));
-	for (int i = 0; i < 52; i++) {
+
+	int i = 0;
+	for (int suit = 0; suit <= SUIT_COUNT; suit++) {
 		for (int rank = 0; rank <= RANK_COUNT; rank++) {
-			for (int suit = 0; suit <= SUIT_COUNT; suit++) {
-				deck[i].rank = rank;
-				deck[i].suit = suit;
-			}
+			deck[i].rank = rank;
+			deck[i].suit = suit;
+			i++;
 		}
 	}
 	return deck;
--- src/main.c
+++ src/main.c
@@ -2,9 +2,7 @@
 
 int main(){
 
-    return 0;
-
-    bool playing;
+    bool playing = true;
     while(playing){
         int ch = getchar();
         if (ch == 'q') playing = false;
@@ -13,15 +11,21 @@ int main(){
             Pile p;
             Card *deck = fill_deck(&p);
 
+            shuffle_pile(deck);
             print_deck(deck);
 
         }
     }
+
+    return 0;
+
 }
 
 void print_deck(Card *deck){
     for (int i = 0; i < 52; i++){
-        printf("|%d %d| ", deck[i].suit, deck[i].rank);
-        if(i/13 == 0) printf("\n");
+        printf("|%d %d|", deck[i].suit, deck[i].rank);
+        if (i != 0 && i%             13 == 0) printf("\n");
+        printf(" ");
     }
+    printf("\n Done!");
 }
