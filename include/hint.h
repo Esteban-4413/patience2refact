1fb64a041631580c12076dcb08aa40f119e2ceda
Fernanda Cidrim <mfcidrimlopes@gmail.com>
2026-08-09 20:40:52 +0100

    Deleted unused functions

--- include/hint.h
+++ include/hint.h
@@ -8,16 +8,11 @@
 
 #define MAX_HINT_DEPTH 3
 
+int evaluate_score(Game_state *state, int src_idx, int dest_idx, int card_count);
+
+void update_score(Game_state *state, int src_idx, int dest_idx, int card_count);
+
 MoveList generate_hints(Game_state *current_state);
 
-Game_state clone_state(Game_state *current_state);
-
-void free_state(Game_state *state);
-
-void swap_hint(MoveEvaluation *moves, int a, int b);
-
-int partition(MoveEvaluation *moves, int N);
-
-void quickSort(MoveEvaluation *moves, int N);
 void print_move_list(const MoveList *move_list);
 #endif
